8c5880fc7347fe72f2b8e2a019d03033560f5474
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 02:55:33 -0500

    create methods and call them from main

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
-public class MethodPractice {
+public class Hello {
+// insert main method
+public static void main(String[] args) {
+    sayHello();
+    sayGoodbye();
+    sayGoodMorning();
+
+
+    }
+    //insert say hello method
+public static void sayHello() {
+        System.out.println("Hello, World!");
+}
+//insert say goodbye method
+public static void sayGoodbye() {
+    System.out.println("Goodbye!");
+}
+//insert say good morning method
+public static void sayGoodMorning() {
+    System.out.println("Hello, Good morning!");
+}
+
 }
